61d1e376c84241db0a835363f34a7b85159303cf
CS <42813301+crs553@users.noreply.github.com>
2025-10-09 23:07:48 +0100

    my initial customisations added

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -73,7 +73,7 @@ vim.keymap.set('n', '<leader>tp', function()
   builtin.find_files()
 end, { desc = '[t]elescope find files - ctrl[p] style' })
 vim.keymap.set('n', '<M-p>', builtin.oldfiles, { desc = '[telescope] old files' })
-vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = '[telescope] live grep' })
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = '[telescope] live grep' })
 vim.keymap.set('n', '<leader>tf', fuzzy_grep, { desc = '[t]elescope [f]uzzy grep' })
 vim.keymap.set('n', '<M-f>', fuzzy_grep_current_file_type, { desc = '[telescope] fuzzy grep filetype' })
 vim.keymap.set('n', '<M-g>', live_grep_current_file_type, { desc = '[telescope] live grep filetype' })
@@ -84,12 +84,12 @@ vim.keymap.set(
   { desc = '[t]elescope grep current string [*] in current filetype' }
 )
 vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = '[telescope] grep current string [*]' })
-vim.keymap.set('n', '<leader>tg', project_files, { desc = '[t]elescope project files [g]' })
-vim.keymap.set('n', '<leader>tc', builtin.quickfix, { desc = '[t]elescope quickfix list [c]' })
-vim.keymap.set('n', '<leader>tq', builtin.command_history, { desc = '[t]elescope command history [q]' })
-vim.keymap.set('n', '<leader>tl', builtin.loclist, { desc = '[t]elescope [l]oclist' })
-vim.keymap.set('n', '<leader>tr', builtin.registers, { desc = '[t]elescope [r]egisters' })
-vim.keymap.set('n', '<leader>tbb', builtin.buffers, { desc = '[t]elescope [b]uffers [b]' })
+vim.keymap.set('n', '<leader>fd', project_files, { desc = '[t]elescope project files [g]' })
+vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = '[t]elescope quickfix list [c]' })
+vim.keymap.set('n', '<leader>fc', builtin.command_history, { desc = '[t]elescope command history [q]' })
+vim.keymap.set('n', '<leader>fbl', builtin.loclist, { desc = '[t]elescope [l]oclist' })
+vim.keymap.set('n', '<leader>fbr', builtin.registers, { desc = '[t]elescope [r]egisters' })
+vim.keymap.set('n', '<leader>fbb', builtin.buffers, { desc = '[t]elescope [b]uffers [b]' })
 vim.keymap.set(
   'n',
   '<leader>tbf',
@@ -110,7 +110,7 @@ telescope.setup {
       'truncate',
     },
     layout_strategy = 'vertical',
-    layout_config = layout_config,
+    --layout_config = layout_config,
     mappings = {
       i = {
         ['<C-q>'] = actions.send_to_qflist,
